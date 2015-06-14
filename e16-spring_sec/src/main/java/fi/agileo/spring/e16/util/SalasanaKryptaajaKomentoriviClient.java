5dec2d13fd6d3e946d111b5213cafd93a2693f18
Jaakko Leikko <jaakko.leikko@haaga-helia.fi>
2015-06-14 20:27:21 +0300

    warning fixes

--- e16-spring_sec/src/main/java/fi/agileo/spring/e16/util/SalasanaKryptaajaKomentoriviClient.java
+++ e16-spring_sec/src/main/java/fi/agileo/spring/e16/util/SalasanaKryptaajaKomentoriviClient.java
@@ -5,10 +5,6 @@ import org.springframework.security.crypto.password.StandardPasswordEncoder;
 
 public class SalasanaKryptaajaKomentoriviClient {
 
-	/**
-	 * @param args
-	 */
-	@SuppressWarnings("resource")
 	public static void main(String[] args) {
 		Scanner lukija = new Scanner(System.in);
 		StandardPasswordEncoder spe = new StandardPasswordEncoder();
@@ -16,8 +12,7 @@ public class SalasanaKryptaajaKomentoriviClient {
 		String salasana = lukija.nextLine();
 		String kryptattuna = spe.encode(salasana);
 		System.out.println("Salasanasi on kryptattuna (random suola mukana): " +kryptattuna);
-		
-
+		lukija.close();
 	}
 
 }
