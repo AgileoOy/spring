a978a38dc80d2b609029a30057564d7edf8b722c
Ismo Harjunmaa <ismo.harjunmaa@gmail.com>
2016-11-30 20:05:07 +0200

    H2 database initialization added

--- e17-rest/database/insert_test_data_mysql.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO henkilo2
-	(etunimi, sukunimi, sahkoposti, lahiosoite, postinumero, postitoimipaikka)
-values
-	('Matti', 'Meikäläinen', 'matti@meikkis.com', 'Puistotie 1', '00330', 'Helsinki'),
-	('Mikko', 'Mallikas', 'mikkomallikas47@hotmail.com', 'Kivakatu 3', '96100', 'Rovaniemi'),
-	('Outi', 'Opiskelija', 'outin.boxi@gmail.com', 'Hoaspolku 4', '00520','Helsinki'),
-	('Tiina', 'Tikolainen', 'tinde@tikotiko.fi', 'Eepostie 7', '02100', 'Espoo');
